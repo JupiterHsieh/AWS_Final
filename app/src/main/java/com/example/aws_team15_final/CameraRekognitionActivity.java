f4df2dda12cc8f4a1c0c319d25be721020103f17
Daofong12 <peter9236@gmail.com>
2023-06-09 16:41:15 +0800

    Grab v2

--- app/src/main/java/com/example/aws_team15_final/CameraRekognitionActivity.java
+++
@@ -1,4 +0,0 @@
-package com.example.aws_team15_final;
-
-public class CameraRekognitionActivity {
-}
--- app/src/main/java/com/example/aws_team15_final/GrabSnackActivity.java
+++ app/src/main/java/com/example/aws_team15_final/GrabSnackActivity.java
@@ -1,53 +1,37 @@
 package com.example.aws_team15_final;
 
-import androidx.activity.result.ActivityResult;
-import androidx.activity.result.ActivityResultCallback;
-import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
-import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
-import android.media.Image;
+import android.graphics.Matrix;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.Toast;
 
-import com.amazonaws.auth.AWSCredentials;
-import com.amazonaws.auth.AWSCredentialsProvider;
-import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.auth.CognitoCachingCredentialsProvider;
-import com.amazonaws.internal.StaticCredentialsProvider;
 import com.amazonaws.regions.Region;
 import com.amazonaws.regions.Regions;
-import com.amazonaws.services.rekognition.AmazonRekognition;
-import com.amazonaws.services.rekognition.AmazonRekognitionClient;
-import com.amazonaws.services.rekognition.model.SearchFacesByImageRequest;
-import com.amazonaws.services.rekognition.model.SearchFacesByImageResult;
 import com.amazonaws.services.s3.AmazonS3Client;
-import com.amazonaws.services.s3.model.S3Object;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class GrabSnackActivity extends AppCompatActivity {
     private static final int CAMERA_REQUEST = 1888;
     private static final int MY_CAMERA_PERMISSION_CODE = 100;
     private ImageView identity_image;
-//    ActivityResultLauncher<Intent> startForResult = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
-//        @Override
-//        public void onActivityResult(ActivityResult result) {
-//            if(result != null && result.getResultCode() == RESULT_OK){
-//
-//            }
-//        }
-//    });
+    public Bitmap upload_image = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,23 +39,24 @@ public class GrabSnackActivity extends AppCompatActivity {
         setContentView(R.layout.activity_grab_snack);
 
         identity_image = findViewById(R.id.identity_imageView);
+        ImageButton camera_btn = findViewById(R.id.camera_button);
         Button verify_btn = findViewById(R.id.verify_button);
 
-//        //network policy
-//        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-//        StrictMode.setThreadPolicy(policy);
-//
-//        // Initialize the Amazon Cognito credentials provider
-//        CognitoCachingCredentialsProvider credentialsProvider = new CognitoCachingCredentialsProvider(
-//                getApplicationContext(),
-//                "us-east-2:5135dfd8-94a5-4210-a9bb-845742f92804", // Identity pool ID
-//                Regions.US_EAST_2 // Region
-//        );
-//
-//        //s3client initialization
-//        final AmazonS3Client s3Client = new AmazonS3Client(credentialsProvider.getCredentials(), Region.getRegion(Regions.US_EAST_1));
+        //network policy
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
 
-        verify_btn.setOnClickListener(new View.OnClickListener() {
+        // Initialize the Amazon Cognito credentials provider
+        CognitoCachingCredentialsProvider credentialsProvider = new CognitoCachingCredentialsProvider(
+                getApplicationContext(),
+                "us-east-2:5135dfd8-94a5-4210-a9bb-845742f92804", // Identity pool ID
+                Regions.US_EAST_2 // Region
+        );
+
+        //s3client initialization
+        final AmazonS3Client s3Client = new AmazonS3Client(credentialsProvider.getCredentials(), Region.getRegion(Regions.US_EAST_1));
+
+        camera_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if(checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
@@ -80,6 +65,40 @@ public class GrabSnackActivity extends AppCompatActivity {
                 else{
                     Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
                     startActivityForResult(cameraIntent,CAMERA_REQUEST);
+                }
+            }
+        });
+
+        verify_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //create a file to write bitmap data
+                File f = new File(getCacheDir(), "test1.jpg");
+                try {
+                    f.createNewFile();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                //Convert bitmap to byte array
+                Bitmap bitmap = upload_image;
+                ByteArrayOutputStream bos = new ByteArrayOutputStream();
+                bitmap.compress(Bitmap.CompressFormat.PNG, 0 /*ignored for PNG*/, bos);
+                byte[] bitmapdata = bos.toByteArray();
+
+                //write the bytes in file && send image to s3-bucket
+                FileOutputStream fos = null;
+                try {
+                    fos = new FileOutputStream(f);
+                    fos.write(bitmapdata);
+                    fos.flush();
+                    fos.close();
+                    s3Client.putObject(getString(R.string.bucket_name), "upload_image.jpg", f);
+                    GrabSnackActivity.this.finish();
+                    Toast.makeText(GrabSnackActivity.this, "Upload success", Toast.LENGTH_LONG).show();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    Toast.makeText(GrabSnackActivity.this, "Upload fail", Toast.LENGTH_LONG).show();
                 }
             }
         });
@@ -110,15 +129,17 @@ public class GrabSnackActivity extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
         if (resultCode == RESULT_OK) {
             Bitmap photo = (Bitmap) data.getExtras().get("data");
+            photo = rotateImage(photo, 90);
             identity_image.setImageBitmap(photo);
-//            upload_image = photo;
-
-//        //try to use rekognition with aws cli using java SDK
-//
-//            AmazonRekognition client = new AmazonRekognitionClient(awsCredentialsProvider);
-//            SearchFacesByImageRequest request = new SearchFacesByImageRequest().withCollectionId("myphotos").withImage()
-//                    .withImage(new Image().withS3Object(new S3Object().withBucket("mybucket").withName("myphoto"))).withMaxFaces(5).withFaceMatchThreshold(95f);
-//            SearchFacesByImageResult response = client.searchFacesByImage(request);
+            upload_image = photo;
         }
+    }
+    //sometime pictures from camera may rotate, this function is used to correct it.
+    public static Bitmap rotateImage(Bitmap source, float angle)
+    {
+        Matrix matrix = new Matrix();
+        matrix.postRotate(angle);
+        return Bitmap.createBitmap(source, 0, 0, source.getWidth(), source.getHeight(),
+                matrix, true);
     }
 }
