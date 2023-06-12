e0dc7213045707c452c04c3cbcd52858e11aaa76
Daofong12 <peter9236@gmail.com>
2023-06-13 01:34:40 +0800

    Debug

--- app/src/main/java/com/example/aws_team15_final/MainActivity.java
+++ app/src/main/java/com/example/aws_team15_final/MainActivity.java
@@ -22,10 +22,6 @@ import com.amazonaws.mobile.auth.ui.AuthUIConfiguration;
 import com.amazonaws.mobile.auth.ui.SignInActivity;
 import com.amplifyframework.AmplifyException;
 import com.amplifyframework.auth.cognito.AWSCognitoAuthPlugin;
-import com.amplifyframework.auth.cognito.result.AWSCognitoAuthSignOutResult;
-import com.amplifyframework.auth.cognito.result.GlobalSignOutError;
-import com.amplifyframework.auth.cognito.result.HostedUIError;
-import com.amplifyframework.auth.cognito.result.RevokeTokenError;
 import com.amplifyframework.core.Amplify;
 import com.amplifyframework.storage.s3.AWSS3StoragePlugin;
 
@@ -93,7 +89,7 @@ public class MainActivity extends AppCompatActivity {
                     Amplify.Auth.signIn(
                             username,
                             password,
-                            result -> isLogin(getApplicationContext(),result.isSignedIn()),
+                            result -> isLogin(getApplicationContext(),result.isSignInComplete()),
                             error -> LoginError(getApplicationContext(),error.toString())
                     );
                 }else{
@@ -127,39 +123,10 @@ public class MainActivity extends AppCompatActivity {
         LogOut();
     }
     private void LogOut(){
-        Amplify.Auth.signOut( signOutResult -> {
-            if (signOutResult instanceof AWSCognitoAuthSignOutResult.CompleteSignOut) {
-                // Sign Out completed fully and without errors.
-                Log.i("AuthQuickStart", "Signed out successfully");
-            } else if (signOutResult instanceof AWSCognitoAuthSignOutResult.PartialSignOut) {
-                // Sign Out completed with some errors. User is signed out of the device.
-                AWSCognitoAuthSignOutResult.PartialSignOut partialSignOutResult =
-                        (AWSCognitoAuthSignOutResult.PartialSignOut) signOutResult;
-
-                HostedUIError hostedUIError = partialSignOutResult.getHostedUIError();
-                if (hostedUIError != null) {
-                    Log.e("AuthQuickStart", "HostedUI Error", hostedUIError.getException());
-                    // Optional: Re-launch hostedUIError.getUrl() in a Custom tab to clear Cognito web session.
-                }
-
-                GlobalSignOutError globalSignOutError = partialSignOutResult.getGlobalSignOutError();
-                if (globalSignOutError != null) {
-                    Log.e("AuthQuickStart", "GlobalSignOut Error", globalSignOutError.getException());
-                    // Optional: Use escape hatch to retry revocation of globalSignOutError.getAccessToken().
-                }
-
-                RevokeTokenError revokeTokenError = partialSignOutResult.getRevokeTokenError();
-                if (revokeTokenError != null) {
-                    Log.e("AuthQuickStart", "RevokeToken Error", revokeTokenError.getException());
-                    // Optional: Use escape hatch to retry revocation of revokeTokenError.getRefreshToken().
-                }
-            } else if (signOutResult instanceof AWSCognitoAuthSignOutResult.FailedSignOut) {
-                AWSCognitoAuthSignOutResult.FailedSignOut failedSignOutResult =
-                        (AWSCognitoAuthSignOutResult.FailedSignOut) signOutResult;
-                // Sign Out failed with an exception, leaving the user signed in.
-                Log.e("AuthQuickStart", "Sign out Failed", failedSignOutResult.getException());
-            }
-        });
+        Amplify.Auth.signOut(
+                () -> Log.i("AuthQuickstart", "Signed out successfully"),
+                error -> Log.e("AuthQuickstart", error.toString())
+        );
     }
 
     private void isLogin(Context context, boolean isSignIn){
