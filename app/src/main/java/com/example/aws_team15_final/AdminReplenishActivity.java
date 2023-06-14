6a91548767d4d8a60d1e8e8addf5adc2ffa1b1db
Daofong12 <peter9236@gmail.com>
2023-06-15 03:28:32 +0800

    replenishment layout

--- app/src/main/java/com/example/aws_team15_final/AdminReplenishActivity.java
+++ app/src/main/java/com/example/aws_team15_final/AdminReplenishActivity.java
@@ -2,14 +2,77 @@
 package com.example.aws_team15_final;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
-public class AdminReplenishActivity extends AppCompatActivity {
+import java.util.ArrayList;
 
+public class AdminReplenishActivity extends AppCompatActivity implements ReplenishRecyclerViewAdapter.ReplenishButtonListener{
+    private RecyclerView replenish_item_recyclerview;
+    ReplenishRecyclerViewAdapter replenishRecyclerViewAdapter;
+    private ArrayList<Integer> test_img_arr = new ArrayList<>();
+    private ArrayList<String> test_replenish_items_arr = new ArrayList<>();
+    private ArrayList<Integer> test_replenish_quantity_arr = new ArrayList<>();
+    private ArrayList<Integer> test_replenish_count_arr = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_replenish);
+        //test
+        test_img_arr.add(R.drawable.noodles);
+        test_img_arr.add(R.drawable.oatbar);
+        test_img_arr.add(R.drawable.doritos);
+        test_img_arr.add(R.drawable.milkpuff);
+        test_img_arr.add(R.drawable.koala);
+        test_img_arr.add(R.drawable.lays);
+        test_replenish_items_arr.add("Noodles");
+        test_replenish_items_arr.add("Oat Bar");
+        test_replenish_items_arr.add("Doritos");
+        test_replenish_items_arr.add("Milk Puff");
+        test_replenish_items_arr.add("Koala");
+        test_replenish_items_arr.add("Lays");
+        test_replenish_quantity_arr.add(12);
+        test_replenish_quantity_arr.add(3);
+        test_replenish_quantity_arr.add(5);
+        test_replenish_quantity_arr.add(0);
+        test_replenish_quantity_arr.add(2);
+        test_replenish_quantity_arr.add(22);
+        test_replenish_count_arr.add(0);
+        test_replenish_count_arr.add(0);
+        test_replenish_count_arr.add(0);
+        test_replenish_count_arr.add(0);
+        test_replenish_count_arr.add(0);
+        test_replenish_count_arr.add(0);
+
+        replenish_item_recyclerview = findViewById(R.id.replenish_recyclerView);
+        GridLayoutManager gridlayoutManager = new GridLayoutManager(AdminReplenishActivity.this, 2);
+        replenish_item_recyclerview.setLayoutManager(gridlayoutManager);
+        replenishRecyclerViewAdapter = new ReplenishRecyclerViewAdapter(test_img_arr,test_replenish_items_arr,test_replenish_quantity_arr,test_replenish_count_arr,this);
+        replenish_item_recyclerview.setAdapter(replenishRecyclerViewAdapter);
+        replenish_item_recyclerview.setHasFixedSize(true);
+    }
+
+    @Override
+    public void minusOnClick(int position) {
+        int replenish_count = test_replenish_count_arr.get(position);
+        // change android side
+        int update_count = replenish_count - 1;
+        test_replenish_count_arr.set(position,update_count);
+    }
+
+    @Override
+    public void plusOnclick(int position) {
+        int replenish_count = test_replenish_count_arr.get(position);
+        // change android side
+        int update_count = replenish_count + 1;
+        test_replenish_count_arr.set(position,update_count);
+    }
+
+    @Override
+    public void replenishOnClick(int position) {
+
     }
 }
---
+++ app/src/main/java/com/example/aws_team15_final/ReplenishRecyclerViewAdapter.java
@@ -0,0 +1,108 @@
+package com.example.aws_team15_final;
+
+import android.annotation.SuppressLint;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class ReplenishRecyclerViewAdapter extends RecyclerView .Adapter<ReplenishRecyclerViewAdapter.replenishViewHolder>{
+    private ArrayList<Integer> test_img_arr;
+    private ArrayList<String> test_replenish_items_arr;
+    private ArrayList<Integer> test_replenish_quantity_arr;
+    private ArrayList<Integer> test_replenish_count_arr;
+    private ReplenishRecyclerViewAdapter.ReplenishButtonListener replenish_buttonListener;
+
+    public ReplenishRecyclerViewAdapter(ArrayList<Integer> test_img_arr,ArrayList<String> test_replenish_items_arr,ArrayList<Integer> test_replenish_quantity_arr,ArrayList<Integer> test_replenish_count_arr,ReplenishButtonListener replenish_buttonListener){
+        this.test_img_arr = test_img_arr;
+        this.test_replenish_items_arr = test_replenish_items_arr;
+        this.test_replenish_quantity_arr = test_replenish_quantity_arr;
+        this.test_replenish_count_arr = test_replenish_count_arr;
+        this.replenish_buttonListener = replenish_buttonListener;
+    }
+    @NonNull
+    @Override
+    public ReplenishRecyclerViewAdapter.replenishViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.replenish_items,parent,false);
+        return new replenishViewHolder(view,replenish_buttonListener);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull replenishViewHolder holder, @SuppressLint("RecyclerView") int position) {
+        holder.replenish_imageView.setImageResource(test_img_arr.get(position));
+        holder.replenish_item_textView.setText(test_replenish_items_arr.get(position));
+        holder.replenish_quantity_textView.setText(test_replenish_quantity_arr.get(position).toString());
+        holder.replenish_count.setText(test_replenish_count_arr.get(position).toString());
+        holder.minus_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(replenish_buttonListener != null){
+                    replenish_buttonListener.minusOnClick(position);
+                    holder.replenish_count.setText(test_replenish_count_arr.get(position).toString());
+                    if(test_replenish_count_arr.get(position) == 0) {
+                        holder.minus_btn.setEnabled(false);
+                    }
+                }
+            }
+        });
+        holder.plus_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(replenish_buttonListener != null){
+                    replenish_buttonListener.plusOnclick(position);
+                    holder.replenish_count.setText(test_replenish_count_arr.get(position).toString());
+                    if(test_replenish_count_arr.get(position) > 0){holder.minus_btn.setEnabled(true);}
+                }
+            }
+        });
+        holder.replenish_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(replenish_buttonListener != null){
+                    replenish_buttonListener.replenishOnClick(position);
+                }
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return test_replenish_items_arr.size();
+    }
+    public static class replenishViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+        ImageView replenish_imageView;
+        TextView replenish_item_textView;
+        TextView replenish_quantity_textView;
+        Button minus_btn;
+        TextView replenish_count;
+        Button plus_btn;
+        Button replenish_btn;
+        public replenishViewHolder(@NonNull View itemView, ReplenishRecyclerViewAdapter.ReplenishButtonListener buttonListener) {
+            super(itemView);
+            replenish_imageView = itemView.findViewById(R.id.replenish_items_imageView);
+            replenish_item_textView = itemView.findViewById(R.id.replenish_items_textView);
+            replenish_quantity_textView = itemView.findViewById(R.id.replenish_quantity_textView);
+            minus_btn = itemView.findViewById(R.id.replenish_quant_minus);
+            replenish_count = itemView.findViewById(R.id.replenish_count);
+            plus_btn = itemView.findViewById(R.id.replenish_quant_plus);
+            replenish_btn = itemView.findViewById(R.id.replenish_button);
+        }
+
+        @Override
+        public void onClick(View v) {
+
+        }
+    }
+    public interface ReplenishButtonListener{
+        void minusOnClick(int position);
+        void plusOnclick(int position);
+        void replenishOnClick(int position);
+    }
+}
